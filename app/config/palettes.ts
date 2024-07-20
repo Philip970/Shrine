96b7a11f275f032034a25a34dcea69db682e27ef
unknown <philip.ewane97@gmail.com>
2024-07-20 16:07:55 +0100

    create files, colors, dimensions, spacings

---
+++ app/config/palettes.ts
@@ -0,0 +1,66 @@
+export enum Colors {
+  primary = "#8e4c37",
+  onPrimary = "#ffffff",
+  primaryContainer = "#ffdbd0",
+  onPrimaryContainer = "#390b01",
+  secondary = "#715952",
+  onSecondary = "#ffffff",
+  secondaryContainer = "#fcdbd2",
+  onSecondaryContainer = "#281712",
+  tertiary = "#685e3c",
+  onTertiary = "#ffffff",
+  tertiaryContainer = "#f1e2b7",
+  onTertiaryContainer = "#221b02",
+  background = "#fffbff",
+  onBackground = "#1f1b19",
+  surface = "#fffbff",
+  onSurface = "#1f1b19",
+  surfaceVariant = "#f1dfda",
+  onSurfaceVariant = "#504441",
+  outline = "#827470",
+}
+
+export enum FontFamilies {
+  avenir = "Avenir",
+  roboto = "Roboto",
+}
+
+export enum FontSizes {
+  xs = 12,
+  sm = 14,
+  base = 16,
+  lg = 20,
+  xl = 24,
+  xxl = 34,
+  xxxl = 48,
+}
+
+export enum FontWeights {
+  xs = "300",
+  sm = "400",
+  base = "500",
+  lg = "600",
+  xl = "700",
+  xxl = "800",
+  xxxl = "900",
+}
+
+export enum Dimensions {
+  xxxs = 16,
+  xxs = 24,
+  xs = 48,
+  sm = 56,
+  lg = 72,
+  xl = 156,
+  xxl = 256,
+  xxxl = 328,
+}
+
+export enum Spacings {
+  xxxs = 4,
+  xxs = 8,
+  xs = 12,
+  sm = 16,
+  md = 24,
+  lg = 32,
+}
